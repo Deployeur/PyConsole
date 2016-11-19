76b6571dacdf92f1174bef10893d5af7ea4c5b80
dugajean <deathmetalgore@gmail.com>
2016-11-19 13:29:59 +0100

    Base application class created. Displays application name and version if not arguments were provided.

---
+++ exec.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+from pyconsole.application import Application
+
+application = Application('Testing CLI', '2.1')
+application.run()
---
+++ pyconsole/application.py
@@ -0,0 +1,21 @@
+import sys
+import getopt
+import typing
+
+class Application:
+	name 	 = 'My First CLI'
+	version  = '1.0'
+	commands = []
+
+	def __init__(self, name, version):
+		self.name = name
+		self.version = version
+
+	def add(self, command: 'Command'):
+		commands.append(command)
+
+	def run(self):
+		if len(sys.argv) == 1:
+			welcome_message = self.name + ' ' + self.version
+			welcome_message += '\n-- Info table here --'
+			return print(welcome_message)
