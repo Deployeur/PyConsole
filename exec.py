6c97a26c53d6616dc102f77d97918866bbdb87e8
dugajean <deathmetalgore@gmail.com>
2016-11-19 13:44:38 +0100

    Exceptions folder added. Command class made ready to work on.

--- exec.py
+++ exec.py
@@ -2,5 +2,11 @@
 
 from pyconsole.application import Application
 
+# Init the application
 application = Application('Testing CLI', '2.1')
+
+# Add the commands
+application.add('strtest')
+
+# Run the app!
 application.run()
--- pyconsole/application.py
+++ pyconsole/application.py
@@ -3,7 +3,7 @@ import getopt
 import typing
 
 class Application:
-	name 	 = 'My First CLI'
+	name = 'My First CLI'
 	version  = '1.0'
 	commands = []
 
@@ -12,10 +12,13 @@ class Application:
 		self.version = version
 
 	def add(self, command: 'Command'):
-		commands.append(command)
+		raise InvalidCommand if !isinstance(command, Command)
+		self.commands.append(command)
 
 	def run(self):
 		if len(sys.argv) == 1:
 			welcome_message = self.name + ' ' + self.version
 			welcome_message += '\n-- Info table here --'
 			return print(welcome_message)
+		else:
+			return print(sys.argv)
